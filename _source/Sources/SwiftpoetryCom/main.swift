54bf140661b0e897890d9e2f0532843a9ac4c5a5
Krzysztof Siejkowski <siejkowski@users.noreply.github.com>
2020-02-22 18:30:01 +0100

    First peek into publish

--- _source/Sources/SwiftpoetryCom/main.swift
+++ _source/Sources/SwiftpoetryCom/main.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Publish
 import Plot
+import ShellOut
 
 // This type acts as the configuration for your website.
 struct SwiftpoetryCom: Website {
@@ -14,15 +15,38 @@ struct SwiftpoetryCom: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://your-website-url.com")!
-    var name = "SwiftpoetryCom"
-    var description = "A description of SwiftpoetryCom"
+    var url = URL(string: "https://swiftpoetry.com")!
+    var name = "swiftpoetry.com"
+    var description = "A description of swiftpoetry.com"
     var language: Language { .english }
     var imagePath: Path? { nil }
+}
+
+public extension DeploymentMethod {
+/// Deploy a website to a given remote using Git.
+/// - parameter remote: The full address of the remote to deploy to.
+    static var copyToRoot: Self {
+        DeploymentMethod(name: "Copy to root") { context in
+            let deployFolder = try context.createDeploymentFolder(
+                withPrefix: "", configure: { _ in }
+            )
+            guard let output = try deployFolder.parent?.parent?.subfolder(named: "Output"),
+                let source = output.parent,
+                let root = source.parent else {
+                fatalError()
+            }
+            let out = try shellOut(
+                to: "mv \(output.path)* \(root.path)",
+                at: source.path
+            )
+            try output.delete()
+            print(out)
+        }
+    }
 }
 
 // This will generate your website using the built-in Foundation theme:
 try SwiftpoetryCom().publish(
     withTheme: .foundation,
-    at: Path("/Users/siejkowski/Projects/swiftpoetry.com/Output")
+    deployedUsing: .copyToRoot
 )
