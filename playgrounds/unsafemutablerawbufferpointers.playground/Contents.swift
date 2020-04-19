900423f7d7aeac2b759ed5c86905dceebc7d3edb
Krzysztof Siejkowski <siejkowski@macbook-pro.home>
2020-04-19 11:08:03 +0200

    Forgot to upload playground 🙄

---
+++ playgrounds/unsafemutablerawbufferpointers.playground/Contents.swift
@@ -0,0 +1,21 @@
+class Us {
+  
+  private var love: Love? = nil
+  
+  var absurd: AndAngst {
+    guard let easedBy = love else {
+      fatalError("must be set before access")
+    }
+    return easedBy
+  }
+}
+
+
+oh! - it.was(such:a:)(hack,
+that).we՚ve(actually: managed)
+to.find(a: setter)
+
+all(the: birches).were(smiling)
+all(the: birches).were(trembling,
+as: we).gave(each: other, the:
+UnsafeMutableRawBufferPointers(to: &ourselves))
---
+++ playgrounds/unsafemutablerawbufferpointers.playground/Sources/Ontology.swift
@@ -0,0 +1,30 @@
+public protocol AndAngst {}
+public protocol Love: AndAngst {}
+public let wonder: Void = ()
+public enum isn՚t { static public func it(a _: Void) {} }
+public let managed: Void = ()
+public let setter: Void = ()
+public enum to { static public func find(a _: Void) -> Birches? { Birches() } }
+public let smiling: Void = ()
+public let trembling: Void = ()
+public struct Birches {
+    public func were(_: Void) -> Birches { self }
+    public func were(_: Void, as _: Void) -> Birches { self }
+    public func gave(each _: Void, the _: UnsafeMutableRawBufferPointers) {}
+}
+public func all(the: Void) -> Birches { Birches() }
+public let birches: Void = ()
+public let other: Void = ()
+public class You {}
+public class Me {}
+public var ourselves: (You, Me) = (You(), Me())
+public let we: Void = ()
+public struct UnsafeMutableRawBufferPointers { public init<T, S>(to: inout (T, S)) {} }
+public let unsafemutablerawbufferpointers: Void = ()
+public let oh: Int? = 1
+public let hack: Void = ()
+public let that: Void = ()
+public struct it {
+    public static func was(such _: Void, a _: Void) -> it { it() }
+    public func we՚ve(actually _: Void) -> Int { 1 }
+}
