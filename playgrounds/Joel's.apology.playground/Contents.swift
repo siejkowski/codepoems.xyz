5b31588d2810482529b3622c0391b59a14b1f7c7
Krzysztof Siejkowski <siejkowski@macbook-pro.home>
2020-05-03 20:07:53 +0200

    forgot playground

---
+++ playgrounds/Joel's.apology.playground/Contents.swift
@@ -0,0 +1,29 @@
+Joelʼs.apology     
+
+
+func == (life: Her, lives: [Other]) -> Bool {
+  guard
+    youʼre != judge(to: choose(life, or: lives)),
+    child != judge(to: choose(life, or: lives)),
+    choose(life, or: lives) == (us.humanity == nil)
+  else { preconditionFailure() }
+  return true
+}
+
+func != (people: [Human], worth: Saving) -> Bool {
+  guard
+    people + fungus == downfall,
+    what(we, became) == animals,
+    her.sacrifice != worth(people)
+  else { preconditionFailure() }
+  return true
+}
+
+func > (love: Love, _: Any) -> Bool {
+  guard I.couldnʼt(loose:her:)(not, again)
+  else { fatalError() }
+  return true
+}
+
+// in Swift, by siejkowski
+
---
+++ playgrounds/Joel's.apology.playground/Sources/Ontology.swift
@@ -0,0 +1,48 @@
+/*
+
+Imagine Joel from the last of us being on trial.
+
+Imagine what apology he'd give.
+
+The most well known apology is Socrates'. It's like this, but Joel's.
+
+What can Joel base his defence on?
+ 
+ * to safe one person is uncomparable to safe them all
+ 
+ * love is the biggest feeling
+ 
+ * he just couldn't do otherwise
+
+*/
+
+
+public enum Joelʼs { public static func apology(tribute _: Void) {} }
+public let last: Void = ()
+public func to(the _: Void, of _: [Human]) {}
+public struct Other: Equatable {}
+public typealias Human = Other
+public let youʼre: Bool = true
+public func judge(to: Bool) -> Bool { to }
+public struct her { public static var sacrifice: Bool = true }
+public typealias Her = her
+public func choose(_: Her, or _: [Human]) -> Bool { true }
+public let child: Bool = true
+public let fungus: [Human] = []
+public let downfall: [Human] = []
+public let we: [Human] = []
+public extension Array where Element == Human {
+    var humanity: Bool? { get { nil } set {} }
+}
+public let us = we
+public let became: Void = ()
+public func what(_: [Human], _: Void) -> Bool { true }
+public let animals: Bool = true
+public typealias Saving = ([Human]) -> Bool
+public struct Love {}
+public let not: Void = ()
+public let again: Void = ()
+public enum I { public static func couldnʼt(loose _: Void, her _: Void) -> Bool { return true }}
+
+//public let Iʼm: Bool = true
+//public let human: Bool = true
